fefe75566abe1308dd704b431c48c23e744d1fd2
Shoujian Zheng <zheng.shoujian@outlook.com>
2023-10-29 21:56:01 +0800

    move pulumi bins to /usr/local/bin

--- .codesandbox/setup-env.sh
+++
@@ -1,6 +0,0 @@
-cat <<EOF | tee -a /root/.zshrc
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-export PATH=\$PATH:/root/.pulumi/bin
-EOF
